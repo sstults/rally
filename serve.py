ca27d869b7cd2d5ab2f8f8ac87487a64c0cf1cf2
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2016-12-02 09:47:02 +0100

    Allow to specify convention leader

--- serve.py
+++ serve.py
@@ -4,4 +4,7 @@ from thespian.actors import ActorSystem
 
 ip_capability = sys.argv[1]
 
-ActorSystem('multiprocTCPBase', capabilities={"ip": ip_capability})
+ActorSystem('multiprocTCPBase', capabilities={
+    "ip": ip_capability,
+    "Convention Address.IPv4": (ip_capability, 1900),
+})
